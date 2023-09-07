2d396c15413f40b7cc4361bf01bbfd165790f4bf
ArtemisMacDuffie <122636675+ArtemisMacDuffie@users.noreply.github.com>
2023-09-07 11:51:13 -0400

    rotator optimization

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotator : MonoBehaviour {
+    private Vector3 rotationAmount = new Vector3(15, 30, 45);
+
     // Update is called once per frame
     void Update() {
-        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        transform.Rotate(rotationAmount * Time.deltaTime);
     }
 }
